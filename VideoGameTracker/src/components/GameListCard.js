9658bbde32dbc0a01189450683ae65ad2d76ac1d
Jeffery <jeffmc825@gmail.com>
2022-01-06 14:06:08 -0600

    pretty much done

--- VideoGameTracker/src/components/GameListCard.js
+++ VideoGameTracker/src/components/GameListCard.js
@@ -17,33 +17,11 @@ function GameListCard({ game, onDelete, onChange, setOnChange}) {
 
     if (seeDetails === true) {
         return (
-<<<<<<< HEAD
-            <div>
-                <article className="gameListCard">
-                    <button className="deleteButton" onClick={handleClick}>Remove from List</button> 
-                    <h3 className="gameListCardTitle">{game.name}</h3>
-                    <img className="gameListCardImage" src={game.background_image} />
-                    <button className="gameListSeeDetails" onClick={handleSeeDetails}> Hide Details</button>
-                    <div className="cardDetails">
-                            <p className="cardRelease">Release Date: {game.released}</p>
-                            {game.metacritic ? <p>Metacritic review: {game.metacritic}</p> : null}
-                            <span style={{textDecoration: "underline"}}>Consoles:</span>
-                            {game.platforms.map((platform, i, platforms) => {
-                                if (i !== platforms.length-1) {
-                                    return <em className="consoles" key={platform.platform.id}>{platform.platform.name}, </em>
-                                }
-                                return <em className="consoles" key={platform.platform.id}>{platform.platform.name} </em>
-                            } )}
-                    </div>
-                    <SwitchButton game={game} onChange={onChange} setOnChange={setOnChange}/>
-                </article>
-            </div>           
-=======
-            <article className="card">
-                <h3>{game.name}</h3>
-                <button className="deleteButton" onClick={handleClick}>x</button> 
-                <img src={game.background_image} />
-                <button className="seeDetails" onClick={handleSeeDetails}> Hide Details</button>
+            <article className="gameListCard">
+                <button className="deleteButton" onClick={handleClick}>Remove From List</button> 
+                <h3 className="gameListCardTitle" >{game.name}</h3>
+                <img className="gameListCardImage" src={game.background_image} />
+                <button className="gameListSeeDetails" onClick={handleSeeDetails}> Hide Details</button>
                 <div className="cardDetails">
                     <p className="cardRelease">Release Date: {game.released}</p>
                     <p>Metacritic review: {game.metacritic}</p>
@@ -57,24 +35,15 @@ function GameListCard({ game, onDelete, onChange, setOnChange}) {
                 </div>
                 <SwitchButton game={game} onChange={onChange} setOnChange={setOnChange}/>
             </article>  
->>>>>>> refs/remotes/origin/main
         )
     }
     else {
         return (
-<<<<<<< HEAD
             <article className="gameListCard">
                 <button className="deleteButton" onClick={handleClick}>Remove From List</button>
                 <h3 className="gameListCardTitle">{game.name}</h3>
                 <img className="gameListCardImage" src={game.background_image} />
                 <button className="gameListSeeDetails" onClick={handleSeeDetails}> See Details</button>
-=======
-            <article className="card">
-                <h3>{game.name}</h3>
-                <button className="deleteButton" onClick={handleClick}>x</button> 
-                <img src={game.background_image} />
-                <button className="seeDetails" onClick={handleSeeDetails}> See Details</button>
->>>>>>> refs/remotes/origin/main
                 <SwitchButton game={game} onChange={onChange} setOnChange={setOnChange}/>
             </article>
         )
--- VideoGameTracker/src/components/MyLists.js
+++ VideoGameTracker/src/components/MyLists.js
@@ -1,20 +1,7 @@
-<<<<<<< HEAD
-// game card
-// three divs, populate gamecards vertically for each div
-
-// potentially add sort functionality
-// rating functionality
-import { React, useState} from "react";
-=======
 import React from "react";
->>>>>>> refs/remotes/origin/main
 import GameListCard from "./GameListCard";
 import RenderGameListCard from "./RenderGameListCard";
 
-<<<<<<< HEAD
-
-=======
->>>>>>> refs/remotes/origin/main
 function MyLists({ gameList, onDelete, onChange, setOnChange }) {
     
     
@@ -25,17 +12,7 @@ function MyLists({ gameList, onDelete, onChange, setOnChange }) {
         <div className="gameListContainer">
             <div className="gameList">
                 <h2 className="gameListHeader">Wishlist</h2>
-<<<<<<< HEAD
                 <RenderGameListCard gameList={gameList} location={"Wishlist"} onDelete={onDelete} onChange={onChange} setOnChange={setOnChange}/>
-=======
-                {gameList.map((game)=> {
-                    if (game.platforms === null) {
-                        return null
-                    } else { if (game.list === "Wishlist") {
-                        return <GameListCard key={game.name} game={game} onDelete={onDelete} onChange={onChange} setOnChange={setOnChange}/>
-                    }}
-                })}
->>>>>>> refs/remotes/origin/main
             </div>
             <div className="gameList">
                 <h2 className="gameListHeader">Playing</h2>
